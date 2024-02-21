655e4d3b4f97c3788d6b14e8f4c2fdb876e7096f
DevPoodle <richardbonilla112@gmail.com>
2024-02-21 08:17:31 -0800

    Add VertexBufferUniform

---
+++ addons/compute-shader-plus/uniforms/vertex-buffer-uniform.gd
@@ -0,0 +1,21 @@
+extends Uniform
+class_name VertexBufferUniform
+
+var vertex_buffer : RID
+var vertex_buffer_size := 0
+
+static func create(vertex_data : PackedByteArray) -> VertexBufferUniform:
+	var uniform := VertexBufferUniform.new()
+	uniform.vertex_buffer_size = vertex_data.size()
+	uniform.vertex_buffer = ComputeHelper.rd.vertex_buffer_create(uniform.vertex_buffer_size, vertex_data, true)
+	return uniform
+
+func get_rd_uniform(binding : int) -> RDUniform:
+	var uniform := RDUniform.new()
+	uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	uniform.binding = binding
+	uniform.add_id(vertex_buffer)
+	return uniform
+
+func _exit_tree() -> void:
+	ComputeHelper.rd.free_rid(vertex_buffer)
